8612459c47714d91a58672d95fa372f52f84b28f
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-06-20 23:24:52 +0200

    init cgi source class

---
+++ CGISource.cpp
@@ -0,0 +1,3 @@
+#include "CGISource.hpp"
+
+CGISource() {}
---
+++ CGISource.hpp
@@ -0,0 +1,17 @@
+#ifndef CGI_SOURCE_HPP
+# define CGI_SOURCE_HPP
+
+# include "Source.hpp"
+
+class CGISource : public Source {
+
+    public:
+
+    private:
+
+        unsigned int    _fd_in;
+        unsigned int    _fd_out;
+
+}
+
+#endif
