fb5b3af5fc54112d1abec616989246682dbef37d
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-07-15 12:21:52 +0200

    remove everything related to a Server class

--- Webserver.hpp
+++ Webserver.hpp
@@ -4,7 +4,6 @@
 # include "webserv.hpp"
 # include "config_parser.hpp"
 # include "Connection.hpp"
-//# include "Server.hpp"
 
 class Webserver {
 
@@ -14,7 +13,6 @@ class Webserver {
 		~Webserver();
 
 		std::vector<configParser::ServerConfig>	&get_config();
-		//std::vector<Server>						&get_servers();
 		std::vector<Connection>					&get_connections();
 		std::vector<pollfd>						&get_polls();
 		std::map<Source &, Connection &>		&get_source_map();
@@ -32,14 +30,13 @@ class Webserver {
 	private:
 
 		std::vector<configParser::ServerConfig>	_config;
-		//std::vector<Server>						_servers;
 		std::vector<Connection>					_connections;
 		std::vector<pollfd>						_polls;
 		std::map<Source &, Connection &>		_source_map;
 
 		void	populate_socket_connections();
 		void	create_polls();
-		//void	create_servers();
+		std::vector<Connection>::iterator _connection_exists(std::vector<configParser::ServerConfig>::iterator it);
 
 		Connection	*find_triggered_socket(pollfd &poll);
 		Connection	*find_triggered_source(pollfd &poll);
